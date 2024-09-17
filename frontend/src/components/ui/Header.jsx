e46d80351a40ba0586bb8ef727230a7ba1df8fa3
skyduranta <akashkr2712@gmail.com>
2024-09-17 20:22:06 +0530

    Deployment completed

--- frontend/src/components/ui/Header.jsx
+++ frontend/src/components/ui/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="mb-10">
-      <h2 className="md:text-6xl text-4xl lg:text-8xl font-bold text-center  relative z-50 text-white pt-10">
+      <h1 className="md:text-6xl text-4xl lg:text-8xl font-bold text-center  relative z-50 text-white pt-10">
         Expense <Link to="/">GQL</Link>
-      </h2>
+      </h1>
       <div className="relative mb-10 w-1/2 mx-auto hidden md:block">
         {/* Gradients */}
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
